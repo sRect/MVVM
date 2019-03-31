0c706d95d0cee8e460b0ffdef69e47e14f41d5df
srect <fcqun@sina.cn>
2019-04-01 00:33:06 +0800

    :construction: feat: 将数据进行代理

--- MVVM.js
+++ MVVM.js
@@ -6,11 +6,23 @@ class MVVM {
     if (this.$el) {
       // 编译之前，对数据进行劫持
       new Observer(this.$data);
+      this.proxyData(this.$data)
       // 用数据和元素进行编译
       new Compile(this.$el, this);
     }
   }
 
-
+  proxyData(data) {
+    Object.keys(data).forEach(key => {
+      Object.defineProperty(this, key, {
+        get() {
+          return data[key];
+        },
+        set(newVal) {
+          data[key] = newVal;
+        }
+      })
+    })
+  }
 
 }
